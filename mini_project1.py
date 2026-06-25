846cb9715635943fc10c404ceed7ea66c711bf70
jajulamanisaipraveen-jpg <jajulamanisaipraveen@gmail.com>
2026-06-25 13:30:56 +0530

    Add files via upload

---
+++ mini_project1.py
@@ -0,0 +1,23 @@
+while True:
+  print("\nTo add a new note enter 1")
+  print("To view the note enter 2")
+  print("To exit enter 3")
+  choice=int(input("Enter choose: "))
+
+  if choice==1:
+    file=open("text.txt","w")
+    file.write(input("Enter note: "))
+    print("Note entered")
+  
+  
+  elif choice==2:
+    file=open("text.txt","r")
+    print(file.read())
+    
+  
+  elif choice==3:
+    print("Bye")
+    break
+
+  else:
+    print("invalid choice")
