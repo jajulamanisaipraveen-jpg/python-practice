b321e13a30075b5fa5f4565c1e68ab70f288ad3b
jajulamanisaipraveen-jpg <jajulamanisaipraveen@gmail.com>
2026-07-15 22:24:56 +0530

    Add files via upload

---
+++ mini_project5.py
@@ -0,0 +1,32 @@
+expenses={}
+while True:
+  print("\n1.Add EXpenses"
+        "\n2.View Expenses"
+        "\n3.Show Total Expenses"
+        "\n4.Exit")
+  print("Please Enter only the numbers")
+  
+  choice=int(input("Ether your choice: "))
+
+  if choice==1:
+    add=input("Enter Expenses: ")
+    add1=int(input("Enter Amount: "))
+    expenses[add]=add1
+    print("Added sucessfuly..")
+
+  elif choice==2:
+    print("\n",expenses)
+
+  elif choice==3:
+    print("\nTotal expenses")
+    print(sum(expenses.values()))
+
+  elif choice==4:
+    print("\nExiting...")
+    break
+
+  else:
+    print("\nPlease enter a correct option..!")
+
+  
+
